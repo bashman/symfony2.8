896c1d071e6b4c90979c2c034b42d9d49b2723ea
bashman <djdaga@gmail.com>
2016-10-17 23:54:01 -0400

    rutas y controladores

---
+++ src/EMM/UserBundle/Controller/DefaultController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace EMM\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('EMMUserBundle:Default:index.html.twig', array('name' => $name));
+    }
+
+
+
+}
